e6ecaf59e5d1089de2b55525d985fb847b2e4c62
Subin <dmsbow_@daum.net>
2023-09-03 11:06:09 +0900

    [Add] ReactQueryProvider선언

---
+++ src/Provider/index.tsx
@@ -0,0 +1,18 @@
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import type {ReactNode} from "react";
+
+const ReactQueryProvider = ( {children}: {children: ReactNode} ) => {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+                staleTime: 5 * 60 * 1000,
+            },
+        }
+    })
+    return (
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    )
+}
+
+export default ReactQueryProvider
